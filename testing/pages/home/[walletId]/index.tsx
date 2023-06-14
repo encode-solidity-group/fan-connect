2105b3ee98675fccb630c8284660adfe053306c3
Jeffrey Cheung <jeffreycheung_@live.com>
2023-06-13 17:37:35 -0700

    restrict posting to creator

--- testing/pages/home/[walletId]/index.tsx
+++ testing/pages/home/[walletId]/index.tsx
@@ -3,20 +3,29 @@ import SideBar from '../../../components/SideBar';
 import Feed from '../../../components/Feed';
 import { useContractRead } from 'wagmi';
 import contractJson from '../../../SubscriptionJson/SubscriptionService.json';
+import { useSession } from 'next-auth/react';
 
 const UserHome = () => {
-  const {data} = useContractRead({
+  const { data: session } = useSession();
+  const userAddress = session?.user?.name;
+
+  const { data } = useContractRead({
     address: '0x2645E09ea0dab2B90C0AbC69c2cAF205b4c152f6',
     abi: contractJson.abi,
-    functionName: '',
-  })
+    functionName: 'creatorPageExists',
+    args: [userAddress],
+  });
 
   return (
     <div className='flex justify-between w-screen'>
       <SideBar />
-      <div className='sm:ml-[175px] lg:ml-[340px] w-full'>
-        <Feed />
-      </div>
+
+      {!data ?
+        <div className='sm:ml-[175px] lg:ml-[340px] w-full'>
+          <Feed />
+        </div>
+        : <></>
+      }
     </div>
   );
 };
