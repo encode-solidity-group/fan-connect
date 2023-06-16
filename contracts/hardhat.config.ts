a06ce9549ae667f80c4ba3c35be95c6bce8a99b2
Jeffrey Cheung <jeffreycheung_@live.com>
2023-06-16 15:08:56 -0700

    deploy to aurora test net

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -2,15 +2,21 @@ import { HardhatUserConfig } from "hardhat/config";
 require('dotenv').config();
 import "@nomicfoundation/hardhat-toolbox";
 
-const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY || "";
-const SEPOLIA_API_KEY = process.env.SEPOLIA_API_KEY || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ALCHEMY_API_KEY = process.env.SEPOLIA_API_KEY || "";
+const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
   networks: {
+    testnet_aurora: {
+      url: `https://aurora-testnet.infura.io/v3/${INFURA_API_KEY}`,
+      chainId: 1313161555,
+      accounts: [PRIVATE_KEY],
+    },
     sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${SEPOLIA_API_KEY}`,
-      accounts: [SEPOLIA_PRIVATE_KEY],
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [PRIVATE_KEY],
     }
   }
 };
