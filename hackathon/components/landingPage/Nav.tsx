9acadac7629c738d1f3eaccf9edb183e3cb54d95
iliacodes <6ac177ed@protonmail.com>
2023-06-20 11:15:21 -0700

    changed navbar

--- hackathon/components/landingPage/Nav.tsx
+++ hackathon/components/landingPage/Nav.tsx
@@ -13,7 +13,7 @@ export default function Header() {
     <nav className="fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50 ">
       <div className="container mx-auto ">
 
-        <div className="w-full  h-[96px] backdrop-blur-md max-w-[360px] mx-auto flex justify-between text-2xl text-red-300 items-center">
+        <div className="w-full  h-[96px] backdrop-blur-md max-w-[360px] mx-auto flex justify-between text-2xl text-[#6BD0FF] items-center">
           <Link className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center" href={'/home'}>
             <AiFillHome />
           </Link>
