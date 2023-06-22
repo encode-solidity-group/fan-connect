f179d4a98f5bbe0279189a29f0a9892c625c2a54
ilam0602 <isaac.lam@live.com>
2023-06-21 17:46:17 -0700

    added default cover/pfp for those that haven't set

--- front-end/components/profile/Username.tsx
+++ front-end/components/profile/Username.tsx
@@ -27,19 +27,19 @@ const Username = ({queryAddress}) => {
             if(userData.profile_picture != undefined) { // set profile picture if it exists
               setProfilePicture(userData.profile_picture);
             } else {
-              setProfilePicture(''); // set to default picture or leave blank if it doesn't exist
+              setProfilePicture('https://firebasestorage.googleapis.com/v0/b/only-blocks.appspot.com/o/images%2Fdefault_pfp.jpeg?alt=media&token=d63ac8a8-49e1-41b0-841f-7c71e2ed69bf'); // set to default picture or leave blank if it doesn't exist
             }
 
             if(userData.cover_photo != undefined) { // set profile picture if it exists
                 setCoverPhoto(userData.cover_photo);
               } else {
-                setCoverPhoto(''); // set to default picture or leave blank if it doesn't exist
+                setCoverPhoto('https://firebasestorage.googleapis.com/v0/b/only-blocks.appspot.com/o/images%2Fdefault_cover.jpeg?alt=media&token=9fb8922f-6418-44b8-9492-4070737a51e3'); // set to default picture or leave blank if it doesn't exist
               }
           }
         } else {
           setUserName(queryAddress);
-          setProfilePicture(''); // set to default picture or leave blank if document doesn't exist
-          setCoverPhoto(''); // set to default picture or leave blank if it doesn't exist
+          setProfilePicture('https://firebasestorage.googleapis.com/v0/b/only-blocks.appspot.com/o/images%2Fdefault_pfp.jpeg?alt=media&token=d63ac8a8-49e1-41b0-841f-7c71e2ed69bf'); // set to default picture or leave blank if document doesn't exist
+          setCoverPhoto('https://firebasestorage.googleapis.com/v0/b/only-blocks.appspot.com/o/images%2Fdefault_cover.jpeg?alt=media&token=9fb8922f-6418-44b8-9492-4070737a51e3'); // set to default picture or leave blank if it doesn't exist
         }
       });
 
