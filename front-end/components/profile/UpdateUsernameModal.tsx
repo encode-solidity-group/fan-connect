fa11c1a64dbe1aede6e6a99969a96699431788a0
ilam0602 <isaac.lam@live.com>
2023-06-21 17:53:41 -0700

    changed cancel button to x on settings popout

--- front-end/components/profile/UpdateUsernameModal.tsx
+++ front-end/components/profile/UpdateUsernameModal.tsx
@@ -96,9 +96,15 @@ const UpdateUsernameModal = ({queryAddress}) => {
         <div className="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
           <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
             <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
-            <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+
             <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+              <button onClick={handleClose} className="absolute top-0 right-0 mt-4 mr-4">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-gray-500 hover:text-gray-700">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              </button>
+              
+              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div className="mt-3 text-center sm:mt-0 sm:text-left">
                   <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">
                     Update Username
@@ -106,40 +112,42 @@ const UpdateUsernameModal = ({queryAddress}) => {
                   <input onChange={handleUsernameChange} type="text" className="mt-3 w-full px-2 py-2 text-gray-700 bg-gray-200 rounded" id="username" name="username" required autoFocus/>
                 </div>
               </div> 
+              
               <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                 <button onClick={handleSave} type="button" className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none sm:ml-3 sm:w-auto sm:text-sm">
                   Save
                 </button>
-                <button onClick={handleClose} type="button" className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
-                  Cancel
-                </button>
               </div>
-            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+
+              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div className="mt-3 text-center sm:mt-0 sm:text-left">
-               <h3 className="text-lg leading-6 font-medium text-gray-900 mt-4" id="modal-title">
+                  <h3 className="text-lg leading-6 font-medium text-gray-900 mt-4" id="modal-title">
                     Upload Profile Picture
                   </h3>
                   <input type="file" onChange={handleFileChange} />
                   <button onClick={handleUpload}>Upload</button>
                   <progress value={progress} max="100" />
+                </div>
               </div>
-              </div>
+
               <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div className="mt-3 text-center sm:mt-0 sm:text-left">
-               <h3 className="text-lg leading-6 font-medium text-gray-900 mt-4" id="modal-title">
+                  <h3 className="text-lg leading-6 font-medium text-gray-900 mt-4" id="modal-title">
                     Upload CoverPhoto
                   </h3>
                   <input type="file" onChange={handleCoverFileChange} />
                   <button onClick={handleCoverUpload}>Upload</button>
                   <progress value={coverProgress} max="100" />
+                </div>
               </div>
-              </div>
+
             </div>
           </div>
         </div>
       )}
     </div>
   );
+ 
 };
 
 export default UpdateUsernameModal;
