6f468df5a90651c04f24862bc72f3faf8acd81c8
Jeffrey Cheung <jeffreycheung_@live.com>
2023-06-22 14:11:01 -0700

    create hook

---
+++ front-end/custom hooks/useBookmarkedPosts.ts
@@ -0,0 +1,27 @@
+import { doc, collection, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
+
+export default function useBookmarkedPosts(userAddress: string | undefined) {
+  const [bookmarkedPosts, setBookmarkedPosts] = useState<string[]>([]);
+
+  useEffect(() => {
+    const fetchBookmarkedPosts = async () => {
+      if (userAddress) {
+        const userRef = doc(collection(db, "users"), userAddress);
+        const userSnap = await getDoc(userRef);
+
+        if (userSnap.exists()) {
+          const userData = userSnap.data();
+          setBookmarkedPosts(userData?.bookmark || []);
+        } else {
+          console.log("No such document!");
+        }
+      }
+    };
+
+    fetchBookmarkedPosts();
+  }, [userAddress]);
+
+  return { bookmarkedPosts };
+}
