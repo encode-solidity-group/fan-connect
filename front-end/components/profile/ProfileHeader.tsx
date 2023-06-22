d94022ac792e9d4f4d83af419f542cced0f606bb
Jeffrey Cheung <jeffreycheung_@live.com>
2023-06-22 13:48:45 -0700

    style connect button

--- front-end/components/profile/ProfileHeader.tsx
+++ front-end/components/profile/ProfileHeader.tsx
@@ -7,12 +7,15 @@ import { useContractRead } from 'wagmi';
 import useGetContractAddress from '../../custom hooks/useGetContractAddress';
 import contractJson from '../../SubscriptionJson/SubscriptionService.json';
 import { UserAddressContext } from '../../providers/UserAddressProvider';
+import { DarkModeContext } from '../../providers/DarkModeProvider';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 export default function ProfileHeader() {
   const { queryAddress } = useContext(QueryAddressContext);
   const { userAddress } = useContext(UserAddressContext);
   const { username, bio, profilePicture, coverPhoto } = useUserInfo(queryAddress);
   const { contractAddress } = useGetContractAddress();
+  const { darkMode } = useContext(DarkModeContext);
 
   const { data: subscriberCount } = useContractRead({
     address: contractAddress,
@@ -27,7 +30,10 @@ export default function ProfileHeader() {
 
   return (
     <div className="border-b border-gray-500 pb-4 w-full">
-      <div className="h-40 bg-cover bg-center" style={{ backgroundImage: `url(${coverPhoto})` }}></div>
+      <div className='-mb-10 flex justify-end mr-8'>
+        <ConnectButton />
+      </div>
+      <div className="h-52 bg-cover bg-center" style={{ backgroundImage: `url(${coverPhoto})` }}></div>
       <div className="flex flex-col -mt-24 mx-4 sm:mx-8">
         <div className='flex'>
           <Image
@@ -35,14 +41,14 @@ export default function ProfileHeader() {
             width={200}
             height={200}
             alt='profile pic'
-            className='aspect-square object-cover rounded-full border-4'
+            className={`aspect-square object-cover rounded-full border-4 ${!darkMode && 'border-gray-500'}`}
           />
-          { userAddress === queryAddress &&
-            <div className='mt-auto -ml-12 border-2 rounded-full'>
+          {userAddress === queryAddress &&
+            <div className={`mt-auto -ml-12 border-2 rounded-full ${!darkMode && 'border-gray-500'}`}>
               <UpdateUsernameModal />
             </div>
           }
-          <div className="text-white mt-auto ml-4 sm:ml-8">
+          <div className="mt-auto ml-4 sm:ml-8">
             <h2 className="text-2xl font-bold">{queryAddress?.slice(0, 4)}...{queryAddress?.slice(38)}</h2>
             <p className="text-lg text-gray-400">@{username === queryAddress ? defaultUsername : username}</p>
             <p className="text-lg">Subscribers: {count}</p>
--- front-end/pages/profile/[id]/index.tsx
+++ front-end/pages/profile/[id]/index.tsx
@@ -1,12 +1,10 @@
 import { useContext } from 'react';
 import SideBar from "../../../components/sidebar/SideBar";
 import ProfileFeed from "../../../components/profile/ProfileFeed";
-import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { QueryAddressContext } from '../../../providers/QueryAddressProvider';
 import { DarkModeContext } from '../../../providers/DarkModeProvider';
 import ProfileHeader from '../../../components/profile/ProfileHeader';
 import SideBarRight from '../../../components/sidebar/RightSideBar';
-import UpdateUsernameModal from '../../../components/profile/UpdateUsernameModal';
 
 function UserProfile() {
   const { queryAddress } = useContext(QueryAddressContext);
@@ -16,9 +14,6 @@ function UserProfile() {
     <div className={`flex justify-between w-screen min-h-screen ${darkMode && 'dark-mode'}`}>
       <SideBar />
       <div className='sm:ml-[175px] lg:ml-[280px] w-full'>
-        <div className='m-4 sm:mr-8 flex justify-end'>
-          <ConnectButton />
-        </div>
         <ProfileHeader />
         <div className='p-4 sm:p-8 w-full'>
           {queryAddress && <ProfileFeed />}
